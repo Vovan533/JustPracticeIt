7502ec4fe4d3710db82d481cb15174f40a758730
Vovan533 <vovan533v@gmail.com>
2022-05-31 16:32:34 +0300

    practica1

---
+++ justsite/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def post_list(request):
+    object_list = Post.published.all() 
+    paginator = Paginator(object_list, 3) # По 3 статьи на каждой странице.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, возвращаем первую страницу.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
