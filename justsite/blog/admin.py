733b5662a0b5e52db85e7e9059a1fa392ff8997c
Vovan533 <vovan533v@gmail.com>
2022-06-03 11:43:33 +0300

    practice 2

--- justsite/blog/admin.py
+++ justsite/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 
 @admin.register(Post)
@@ -11,4 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
 
